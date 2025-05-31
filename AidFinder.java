f1e767c036fd4f47bd4dd4ff95e9d50f5df8d119
Kritika Doshi <kritikadoshi1602@gmail.com>
2025-05-31 11:41:57 -0700

    Create AidFinder.java

---
+++ AidFinder.java
@@ -0,0 +1,7 @@
+public class AidFinder {
+    public void listScholarships() {
+        System.out.println("1. Pell Grant - Need-based aid");
+        System.out.println("2. STEM Scholars - GPA > 3.5, STEM major");
+        System.out.println("3. First-Gen Fund - For first-generation college students");
+    }
+}
